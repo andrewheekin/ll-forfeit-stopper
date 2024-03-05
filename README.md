fdd48e14f75c2959ffed404be21ec2cfb24bfd26
Andrew <andrewheekin@gmail.com>
2024-03-04 21:12:30 -0500

    Dockerfile and AWS Lambda building from AWS ECR docker container

--- README.md
+++ README.md
@@ -3,6 +3,18 @@
 To use Selenium along with a ChromeDriver in a Serverless Framework project for AWS Lambda, you indeed need to handle Python dependencies correctly and include the ChromeDriver binary in your deployment package. The serverless-python-requirements plugin is essential for this, as it bundles your Python dependencies, including Selenium, correctly for Lambda. Additionally, you'll need to include the ChromeDriver and a headless Chrome browser binary in your deployment because AWS Lambda does not have these installed.
 
 
+## Deploy
+`sls deploy`
+
+
+## Invoke
+`sls invoke -f check-ll-submission`
+
+
+## Remove
+https://www.serverless.com/framework/docs/providers/aws/cli-reference/remove
+`sls remove`
+
 
 
 ## Run
@@ -16,6 +28,11 @@ To use Selenium along with a ChromeDriver in a Serverless Framework project for 
 ```
 
 
-## TODO
-1. deploy to AWS
-2. 
+## Guides
+
+### Using Environment Variables
+https://www.serverless.com/framework/docs/environment-variables
+To use environment variables in your serverless.yml file, you can use the ${env:MY_ENV_VAR} syntax. This is useful for referencing environment variables in your serverless.yml file, or for using environment variables in your Lambda functions.
+```
+useDotenv: true
+```
--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import os
-import platform
+import requests
+import json
 import argparse
+from tempfile import mkdtemp
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver import ChromeOptions
 import boto3
 from twilio.rest import Client
 from dotenv import load_dotenv
@@ -25,37 +26,53 @@ PHONE_NUMBER = os.getenv("PHONE_NUMBER")
 SMS_BODY = os.getenv("SMS_BODY")
 SITE_URL = os.getenv("SITE_URL")
 AWS_REGION = os.getenv("AWS_REGION")
+INNER_TEXT = os.getenv("INNER_TEXT")
+GROUPME_API_URL = os.getenv("GROUPME_API_URL")
+GROUPME_BOT_ID = os.getenv("GROUPME_BOT_ID")
 
 
-def setup_driver(headless=False):
-    # chrome_options = Options()
-    chrome_options = ChromeOptions()
+def setup_driver_serverless():
+    options = webdriver.ChromeOptions()
+    service = webdriver.ChromeService("/opt/chromedriver")
+
+    options.binary_location = '/opt/chrome/chrome'
+    options.add_argument("--headless=new")
+    options.add_argument('--no-sandbox')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1280x1696")
+    options.add_argument("--single-process")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-tools")
+    options.add_argument("--no-zygote")
+    options.add_argument(f"--user-data-dir={mkdtemp()}")
+    options.add_argument(f"--data-path={mkdtemp()}")
+    options.add_argument(f"--disk-cache-dir={mkdtemp()}")
+    options.add_argument("--remote-debugging-port=9222")
+
+    chrome = webdriver.Chrome(options=options, service=service)
+    print("Headless Chrome driver initialized")
+    return chrome
+
+
+def setup_driver_mac(headless=False):
+    options = webdriver.ChromeOptions()
+
     if headless:
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-gpu")
-        # chrome_options.binary_location = "/opt/homebrew/bin/chromedriver" # Path to headless chromium binary
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-gpu")
 
-    # Determine the correct ChromeDriver based on OS and potentially architecture
-    driver_filename = ""
-    if platform.system() == 'Linux':
-        driver_filename = "chromedriver-122-linux64"
-    elif platform.system() == 'Darwin':  # macOS
-        # Optionally, you might differentiate by architecture (e.g., ARM vs. Intel) if necessary
-        driver_filename = "chromedriver-122-mac-arm64"
-    
     # Construct the path to the ChromeDriver
-    webdriver_path = os.path.join(os.path.dirname(__file__), 'webdrivers', driver_filename)
-
-    print(f"Platform: {platform.system()}")
+    webdriver_path = os.path.join(os.path.dirname(
+        __file__), 'webdrivers', 'chromedriver-122-mac-arm64')
     print(f"Using ChromeDriver located at: {webdriver_path}")
     # Ensure the ChromeDriver has executable permissions
     os.chmod(webdriver_path, 0o755)
 
     # Specify the path to ChromeDriver using the Service object
     service = Service(executable_path=webdriver_path)
-
-    return webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(service=service, options=options)
+    return driver
 
 
 def access_ll_site(driver):
@@ -77,6 +94,7 @@ def login(driver, username, password):
         username_field.send_keys(username)
         password_field.send_keys(password)
         submit_button.click()
+        print("Login successful. Checking submission for today.")
         return True
     except TimeoutException:
         print("Timed out waiting for login elements to appear.")
@@ -87,10 +105,11 @@ def check_todays_submission(driver):
     has_submitted_today = False
 
     try:
-        # Wait up to 10 seconds for the div with inner text "LearnedLeague" to be present.
+        # Wait up to 10 seconds for the div with inner text to be present.
         # Note, this will be present on the page regardless of whether the user has submitted for the day.
         ll_element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//div[text()='LearnedLeague']"))
+            EC.presence_of_element_located(
+                (By.XPATH, f"//div[text()='{INNER_TEXT}']"))
         )
 
         element = driver.find_element(By.CLASS_NAME, "no_sub")
@@ -101,24 +120,24 @@ def check_todays_submission(driver):
         else:
             has_submitted_today = True
 
+        print(f"User {'has' if has_submitted_today else 'has NOT'} submitted today.")
         return has_submitted_today
     except:
         # If the div is not found or any other exception occurs, assume submission has occurred
         has_submitted_today = True
 
+        print(f"User {'has' if has_submitted_today else 'has NOT'} submitted today.")
         return has_submitted_today
 
 
-
-def send_twilio_sms_message():
+def send_twilio_sms_message(phone_number, message):
     client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     message = client.messages.create(
-        body=SMS_BODY,
+        body=message,
         from_=TWILIO_PHONE_NUMBER,
-        to=PHONE_NUMBER
+        to=phone_number
     )
-    print(f"Message sent: {message.sid}")
-
+    return message
 
 
 def send_aws_sns_sms_message(phone_number, message):
@@ -134,24 +153,51 @@ def send_aws_sns_sms_message(phone_number, message):
     return response
 
 
+def send_groupme_message(message):
+    """
+    Send a message to a GroupMe group using the bot API.
+
+    Equivalent to the following curl command:
+    curl -d '{"text" : GROUPME_MESSAGE, "bot_id" : GROUPME_BOT_ID}' GROUPME_API_URL
+    """
+    try:
+        data = {
+            "text": message,
+            "bot_id": GROUPME_BOT_ID
+        }
+        headers = {
+            "Content-Type": "application/json"
+        }
+        response = requests.post(
+            GROUPME_API_URL, data=json.dumps(data), headers=headers)
+        response.raise_for_status()
+        print(f"Message sent to GroupMe: {message}")
+        return response
+    except requests.exceptions.RequestException as e:
+        print(f"Error sending message to GroupMe: {e}")
+
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Run Selenium script in different modes.")
-    parser.add_argument("-i", "--interactive", action="store_true", help="Run script in interactive mode")
+    parser = argparse.ArgumentParser(
+        description="Run Selenium script in different modes.")
+    parser.add_argument("-i", "--interactive", action="store_true",
+                        help="Run script in interactive mode")
     # Use -H instead of -h for headless mode to avoid conflict with the help option
-    parser.add_argument("-H", "--headless", action="store_true", help="Run Chrome in headless mode")
+    parser.add_argument("-H", "--headless", action="store_true",
+                        help="Run Chrome in headless mode")
     args = parser.parse_args()
-    
+
     # Check if both interactive and headless flags are set
     if args.interactive and args.headless:
-        parser.error("The options -i/--interactive and -H/--headless cannot be used together.")
-    
+        parser.error(
+            "The options -i/--interactive and -H/--headless cannot be used together.")
+
     return args
 
 
 def main():
     args = parse_arguments()
-    driver = setup_driver(headless=args.headless)
+    driver = setup_driver_mac(headless=args.headless)
 
     if args.interactive:
         while True:
@@ -169,28 +215,26 @@ def main():
                 login(driver, USERNAME, PASSWORD)
             elif choice == "3":
                 result = check_todays_submission(driver)
-                print(f"User has submitted today: {result}")
+                send_groupme_message(
+                    "You've submitted LL" if result else "🚨 Please submit LL today :)")
             elif choice == "4":
                 print("Sending message...")
-                # send_twilio_sms_message()
+                # response = send_twilio_sms_message(PHONE_NUMBER, SMS_BODY)
                 # response = send_aws_sns_sms_message(PHONE_NUMBER, SMS_BODY)
-                # print(f"Message sent: {response}")
-                
             elif choice == "0":
+                print("Exiting...")
                 if driver:
                     driver.quit()
-                print("Exiting...")
                 break
             else:
                 print("Invalid choice. Please enter a valid option.")
     else:
         access_ll_site(driver)
         if login(driver, USERNAME, PASSWORD):
-            print("Login successful. Checking submission for today.")
             exists = check_todays_submission(driver)
             if not exists:
                 print("Submission not found. Sending reminder text message.")
-                # send_twilio_sms_message()
+                # response = send_twilio_sms_message(PHONE_NUMBER, SMS_BODY)
                 # response = send_aws_sns_sms_message(PHONE_NUMBER, SMS_BODY)
                 # print(f"Message sent: {response}")
             else:
@@ -198,6 +242,31 @@ def main():
         else:
             print("Login failed or page did not load properly.")
         driver.quit()
+
+
+def handler(event=None, context=None):
+    # Serverless function entry point (ex AWS Lambda), headless mode is enabled by default
+    driver = setup_driver_serverless()
+    access_ll_site(driver)
+    if login(driver, USERNAME, PASSWORD):
+        exists = check_todays_submission(driver)
+        if not exists:
+            print("Submission not found. Sending reminder text message.")
+            # response = send_twilio_sms_message(PHONE_NUMBER, SMS_BODY)
+            # response = send_aws_sns_sms_message(PHONE_NUMBER, SMS_BODY)
+            # print(f"Message sent: {response}")
+
+            # Send a message to a GroupMe group
+            send_groupme_message("🚨 Please submit LL today :)")
+        else:
+            print("Submission found. No action needed.")
+
+            # 2024-03-04 Worked, no need for this message
+            # send_groupme_message("You've submitted LL")
+    else:
+        print("Login failed or page did not load properly.")
+    driver.quit()
+    # return {"status": "success"}
 
 
 if __name__ == "__main__":
